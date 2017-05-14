fcd8cfbc240818c977b25e440ff3677d56be940a
Grader <labcontext@gmail.com>
2017-05-14 22:00:38 +0500

    frame http request

--- src/main/java/controller/ToBet.java
+++ src/main/java/controller/ToBet.java
@@ -1,4 +1,48 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ToBet {
+
+    public static void main(String[] args) {
+
+        String query = "http://4pda.ru";
+
+        HttpURLConnection connection = null;
+        try {
+            connection = (HttpURLConnection) new URL(query).openConnection();
+
+            connection.setRequestMethod("GET");
+            connection.setUseCaches(false);
+            connection.setConnectTimeout(250);
+            connection.setReadTimeout(250);
+
+            connection.connect();
+
+            StringBuilder sb = new StringBuilder();
+
+            if (HttpURLConnection.HTTP_OK == connection.getResponseCode()) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "cp1251"));
+
+                String line;
+                while ((line = in.readLine()) != null) {
+                    sb.append(line);
+                    sb.append("\n");
+                }
+
+                System.out.println(sb.toString());
+            } else {
+                System.out.println("Fail " + connection.getResponseCode() + ", " + connection.getResponseMessage());
+            }
+        } catch (Throwable cause) {
+            cause.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
 }
